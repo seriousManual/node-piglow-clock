fbfbe2e63e2b53f14daf6c65e3c26406a98a1c1a
zaphod1984 <zaphod84@gmx.de>
2013-10-09 22:53:17 +0200

    brightness parameter

--- bin/piglowWatch.js
+++ bin/piglowWatch.js
@@ -3,6 +3,8 @@
 var argv = require('optimist').argv;
 var piGlowWatch = require('../');
 
+var options = {};
+
 if(argv.h || argv.help) {
     showHelp();
     process.exit(0);
@@ -13,7 +15,11 @@ if(argv.v || argv.version) {
     process.exit(0);
 }
 
-piGlowWatch.start({}, function(error) {
+if(argv.b || argv.brightness) {
+    options.brightness = argv.b || argv.brightness;
+}
+
+piGlowWatch.start(options, function(error) {
     if(error) {
         console.log(error.message);
         process.exit(1);
@@ -28,4 +34,24 @@ function end() {
     setTimeout(function() {
         process.exit(0);
     }, 1000);
+}
+
+function showVersion() {
+    console.log(require('../package.json').version);
+}
+
+function showHelp() {
+    var help = [
+        'Usage: piglow-watch [options]',
+        '',
+        'Options:',
+        '  -b, --brightenss: sets the brightness of the LEDs',
+        '  -h, --help:       this help',
+        '  --24hours:        set the 24hours format (default: true)',
+        '  --12hours:        set the 12hours format (default: false)',
+        '',
+        ''
+    ];
+
+    console.log(help.join('\n'));
 }
